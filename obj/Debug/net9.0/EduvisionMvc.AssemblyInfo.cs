7fabfef980a6eb5901014b552b94581767caa18d
rajkarthikmanam <rajkarthikmanam@usf.edu>
2025-11-12 15:20:47 -0500

    Simplify trend endpoint to avoid LINQ GroupBy

--- obj/Debug/net9.0/EduvisionMvc.AssemblyInfo.cs
+++ obj/Debug/net9.0/EduvisionMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduvisionMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b371443f418ee1045dec1de7631a0e8fbfeba9a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52f3020434b05b302d8f8426ba68c099d1d30a13")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduvisionMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduvisionMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
