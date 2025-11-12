9f50dfcd642c6a56c04d31803693d72fd85d36fe
rajkarthikmanam <rajkarthikmanam@usf.edu>
2025-11-12 14:57:59 -0500

    Enable developer exception page in Development mode

--- Program.cs
+++ Program.cs
@@ -138,6 +138,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
